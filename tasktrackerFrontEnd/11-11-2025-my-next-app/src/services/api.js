f9cd583403ad93cc5cf52a105babce5e510093a1
SahilVishwakarma7922 <vsahil79222@gmail.com>
2026-06-12 08:18:06 +0530

    task tracker front and back

--- tasktrackerFrontEnd/11-11-2025-my-next-app/src/services/api.js
+++ tasktrackerFrontEnd/11-11-2025-my-next-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://192.168.0.103:8080/api/v1",
+    baseURL: "http://192.168.1.105:8080/api/v1",
 });
 
 export default API;
